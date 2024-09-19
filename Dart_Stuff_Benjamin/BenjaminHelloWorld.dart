a2bbcb9c6d73442491990dd0c99b5f4b1d51cdf7
BenjaminOkwiBenji <jamainben@gmail.com>
2024-09-19 21:18:03 +0300

    Update BenjaminHelloWorld.dart
    Committing added Area computation functionality

--- Dart_Stuff_Benjamin/BenjaminHelloWorld.dart
+++ Dart_Stuff_Benjamin/BenjaminHelloWorld.dart
@@ -1,4 +1,20 @@
 
 void main() {
-  print('Dart Welcomes Benjamin to, Hello World!');
+  
+  /* Simple Welcoming  Hello, World! message*/
+  
+print('Dart Welcomes Benjamin to, Hello World!');
+
+print('A simple program to compute area with a given radius');
+
+
+   const Pi = 3.14; 
+   int Radius=5;
+   double Area = Pi*Radius*Radius; 
+
+print('If the radius is given by ${Radius}');
+   print("Then the area output is ${Area}" ); 
+}
+  
+  
 }
